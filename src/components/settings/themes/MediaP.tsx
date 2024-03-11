29bc405c6e7d1191d85688100c90164dda3d4943
André Oliveira <oliveira.andrerodrigues95@gmail.com>
2024-03-11 12:25:05 +0000

    More styling

--- src/components/settings/themes/MediaP.tsx
+++ src/components/settings/themes/MediaP.tsx
@@ -1,20 +1,30 @@
 import { useEffect, useState } from 'react';
 import {
+  Button,
   Card,
   CardActions,
-  CardContent,
   CardHeader,
   CardMedia,
+  CssBaseline,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogProps,
+  DialogTitle,
+  IconButton,
   Pagination,
-  PaginationItem,
-  Typography,
 } from '@mui/material';
+import InstallDesktopIcon from '@mui/icons-material/InstallDesktop';
+import InfoIcon from '@mui/icons-material/Info';
+import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
 import { ITheme } from '../../../models/Theme';
 
 interface IProps {
   themes: ITheme[];
   searchTerm: string;
 }
+
+const DIALOG_HEIGHT = 600;
 
 export default function MediaP(props: IProps) {
   const { themes: initThemes, searchTerm } = props;
@@ -53,14 +63,17 @@ export default function MediaP(props: IProps) {
 
   const currentThemes = themes.slice(start, end);
 
-  // const isSelected = (theme: ITheme) => {
-  //   if (theme.id === 6) {
-  //     console.log('selected', theme.id);
-  //     return true;
-  //   }
+  const [selectedTheme, setSelectedTheme] = useState<ITheme | null>(null);
+  const [dialogOpen, setDialogOpen] = useState(false);
 
-  //   return false;
-  // };
+  const handleInfoClick = (theme: ITheme) => {
+    setSelectedTheme(theme);
+    setDialogOpen(true);
+  };
+
+  const handleCloseDialog = () => {
+    setDialogOpen(false);
+  };
 
   return (
     <>
@@ -69,11 +82,7 @@ export default function MediaP(props: IProps) {
         page={page}
         onChange={handlePageChange}
         color="primary"
-        sx={{
-          '& > .MuiPagination-ul': {
-            justifyContent: 'center',
-          },
-        }}
+        className="pagination"
       />
       <div
         style={{
@@ -88,44 +97,86 @@ export default function MediaP(props: IProps) {
           return (
             <Card
               key={theme.id}
-              sx={{
-                margin: '1rem',
-                display: 'block',
-                flexDirection: 'column',
-                justifyContent: 'space-between',
-              }}
+              className={`card ${theme.id === 6 ? 'selected' : ''}`}
             >
-              <CardHeader
-                title={theme.name}
-                subheader={theme.version}
-                sx={{ height: 'min-content' }}
-              />
-              <CardMedia
-                component="img"
-                height="100"
-                src={theme.preview ?? 'https://via.placeholder.com/150'}
-                alt={theme.name}
-              />
-              <CardActions>
-                <button>Install</button>
-                <button>Preview</button>
+              <div
+                onClick={() => handleInfoClick(theme)}
+                onKeyDown={() => handleInfoClick(theme)}
+                role="button"
+                tabIndex={0}
+              >
+                <CardHeader
+                  title={theme.name}
+                  subheader={theme.version}
+                  sx={{ height: 'min-content' }}
+                />
+                <CardMedia
+                  component="img"
+                  height="100"
+                  src={theme.preview ?? 'https://via.placeholder.com/150'}
+                  alt={theme.name}
+                />
+              </div>
+              <CardActions sx={{ justifyContent: 'center' }}>
+                <IconButton aria-label="install">
+                  <InstallDesktopIcon />
+                </IconButton>
+                <IconButton
+                  aria-label="info"
+                  onClick={() => handleInfoClick(theme)}
+                >
+                  <InfoIcon />
+                </IconButton>
               </CardActions>
-              {/* <CardContent>
-              <h3>{theme.name}</h3>
-            </CardContent> */}
-              {/* <img
-              src={theme.preview ?? 'https://via.placeholder.com/150'}
-              alt={theme.name}
-              style={{ width: '100%', height: 'auto' }}
-            />
-            <div>
-              <h3>{theme.name}</h3>
-              <p>{theme.description}</p>
-            </div> */}
             </Card>
           );
         })}
       </div>
+
+      {/* Dialog for Theme Info */}
+      <Dialog
+        open={dialogOpen}
+        onClose={handleCloseDialog}
+        className="dialog"
+        sx={{
+          zIndex: 10_000,
+          height: DIALOG_HEIGHT,
+          alignSelf: 'center',
+          top: -DIALOG_HEIGHT * 2,
+        }}
+        scroll="paper"
+      >
+        <DialogTitle>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
+            {selectedTheme?.name}
+            <div>
+              <IconButton aria-label="install">
+                <InstallDesktopIcon />
+              </IconButton>
+              <IconButton aria-label="uninstall">
+                <DeleteForeverOutlinedIcon />
+              </IconButton>
+            </div>
+          </div>
+        </DialogTitle>
+        <DialogContent>
+          {/* Display specific data for the selected theme here */}
+          <CardMedia
+            component="img"
+            height="300"
+            src={selectedTheme?.preview ?? 'https://via.placeholder.com/150'}
+            alt={selectedTheme?.name}
+          />
+          <p>Version: {selectedTheme?.version}</p>
+          <p>{selectedTheme?.description}</p>
+        </DialogContent>
+      </Dialog>
     </>
   );
 }
